5560a761264599233a6cba2e5d3d09136242db92
Carbon <robotcreeper13@gmail.com>
2021-02-27 02:59:50 -0600

    Add 404 OG

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,23 @@
 import { Session } from "next-auth/client";
 import NotFoundIllustration from "../components/illustrations/404";
 import Navbar from "../components/Navbar";
+import Head from "next/head";
 
 export default function NotFound({ session }: {
   session: Session,
 }) {
   return (
     <div className="h-screen w-screen bg-gray-900 text-center">
+        <Head>
+            <title>Page Not Found | Snacc</title>
+            <meta property={"og:type"} content={"website"}/>
+            <meta property={"og:site_name"} content={"Snacc"}/>
+            <meta property={"og:url"} content={"https://snacc.chat"}/>
+            <meta property={"og:locale"} content={"en-US"}/>
+            <meta property={"og:title"} content={"Snacc | Some New Advanced Coder Chat"}/>
+            <meta property={"og:description"} content={"An open source social media network for programmers"}/>
+            <meta property={"description"} content={"An open source social media network for programmers"}/>
+        </Head>
       <Navbar session={session} activePage={""} />
       <div className="flex justify-center my-10 mx-3">
         <NotFoundIllustration width="500" height="500" />
