6de7d5e3df65d4028ba194d59079280324101341
Jack Merrill <yoshifan509@gmail.com>
2021-02-27 01:53:47 -0600

    fix: vercel should work now

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import { GetServerSideProps } from "next";
 import { getSession, Session } from "next-auth/client";
-import { NextApiRequest } from "next-auth/_utils";
-import Head from "next/head";
 import Navbar from "../components/Navbar";
-import styles from "../styles/Home.module.css";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context)
