fdc21deaa89dd4fdfc6d49ab3deb22b8bcb28df3
Jack Merrill <yoshifan509@gmail.com>
2021-02-27 01:45:38 -0600

    fix: fix typeerror in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -67,7 +67,7 @@ export default function Navbar({ session, activePage="" }: {
                                     </button>
                                 </div>
                                 <div className={userMenuOpen ? "origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5" : "hidden"} role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
-                                    <Link href={session.user.name}>
+                                    <Link href={`/users/`}>
                                         <a className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Your Profile</a>
                                     </Link>
                                     <Link href="/settings" passHref>
